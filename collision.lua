da75bb45bf367f6b5f4d4e61a285cdab3cd3a8ac
James Rhodes <jarhodes314@gmail.com>
2017-12-01 17:12:12 +0000

    Brkone: collisions

--- collision.lua
+++ collision.lua
@@ -9,21 +9,25 @@ function collisionUpdate()
   end  
 end
 
+function objCollision(obj1, obj2)
+  return AABB(obj1.x,obj1.y,obj1.width,obj1.height, obj2.x, obj2.y, obj2.width, obj2.height)
+end
+
 function updateObject(obj)
-  for i = 0, obj.w, 16 do 
-    if get(math.floor((obj.x + i)/16),obj.y) = 0 and obj.vel.y < 0 then
+  for i = 0, obj.width, 16 do
+    if get(math.floor((obj.x + i)/16)+1,math.floor(obj.y/16)+1) ~= 0 and obj.vel.y < 0 then
       obj.vel.y = 0
     end
-    if get(math.floor((obj.x + i)/16),obj.y+obj.height) = 0 and obj.vel.y > 0 then
+    if get(math.floor((obj.x + i)/16)+1,math.floor((obj.y+obj.height)/16)+1) ~= 0 and obj.vel.y > 0 then
       obj.vel.y = 0
     end
   end
  
-  for i = 0, obj.h, 16 do 
-    if get(math.floor((obj.y + i)/16),obj.x) = 0 and obj.vel.x < 0 then
+  for i = 0, obj.height, 16 do 
+    if get(math.floor((obj.y + i)/16)+1,math.floor(obj.x/16)+1) ~= 0 and obj.vel.x < 0 then
       obj.vel.x = 0
     end
-    if get(math.floor((obj.y + i)/16),obj.x+obj.height) = 0 and obj.vel.x > 0 then
+    if get(math.floor((obj.y + i)/16)+1,math.floor((obj.x+obj.height)/16)+1) ~= 0 and obj.vel.x > 0 then
       obj.vel.x = 0
     end
   end 
