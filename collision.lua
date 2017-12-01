7218eacfb3662c0f16ec19e74b81fa4c7db4f05b
James Rhodes <jarhodes314@gmail.com>
2017-12-01 17:21:50 +0000

    More collisions

--- collision.lua
+++ collision.lua
@@ -1,8 +1,8 @@
 function collisionUpdate()
-  updateObject(player)
+  updateObject(player, 0)
   for i = #enemies, 1 , -1 do
     thisEnemy = enemies[i]
-    updateObject(thisEnemy)
+    updateObject(thisEnemy, -thisEnemy.vel)
     if objCollision(player,thisEnemy) then
       playerHurt()
     end
@@ -13,22 +13,22 @@ function objCollision(obj1, obj2)
   return AABB(obj1.x,obj1.y,obj1.width,obj1.height, obj2.x, obj2.y, obj2.width, obj2.height)
 end
 
-function updateObject(obj)
+function updateObject(obj,newVel)
   for i = 0, obj.width, 16 do
     if get(math.floor((obj.x + i)/16)+1,math.floor(obj.y/16)+1) ~= 0 and obj.vel.y < 0 then
-      obj.vel.y = 0
+      obj.vel.y = -newVel.y
     end
     if get(math.floor((obj.x + i)/16)+1,math.floor((obj.y+obj.height)/16)+1) ~= 0 and obj.vel.y > 0 then
-      obj.vel.y = 0
+      obj.vel.y = -newVel.y
     end
   end
  
   for i = 0, obj.height, 16 do 
     if get(math.floor((obj.y + i)/16)+1,math.floor(obj.x/16)+1) ~= 0 and obj.vel.x < 0 then
-      obj.vel.x = 0
+      obj.vel.x = -newVel.x
     end
     if get(math.floor((obj.y + i)/16)+1,math.floor((obj.x+obj.height)/16)+1) ~= 0 and obj.vel.x > 0 then
-      obj.vel.x = 0
+      obj.vel.x = -newVel.x
     end
   end 
 end
