b636c74404787c4dc3390d246b85ef1db26a8dfc
Anik Roy <anik545@gmail.com>
2017-12-01 16:27:43 +0000

    .

--- enemies.lua
+++ enemies.lua
@@ -1,18 +1,20 @@
 enemies = {}
 
 function enemyLoad()
-  enemy = {SS = love.graphics.newImage("assets/img/enemies/fish-big.png")}
+  enemy = {}
+  enemy.SS = love.graphics.newImage("assets/img/enemies/fish-big.png")
   enemy.quads = {}
   enemy.sprite = {w = 54, h = 49} 
   enemy.drawCount = 0
   enemy.updateTime = 10
+  enemy.height = 
 
   enemies = {}
 
   table.insert(enemies, {x = 50, y = 50})
 
   for x=0, 3 do
-    table.insert(enemy.quads,love.graphics.newQuad(x*54,0,enemy.sprite.w,enemy.sprite.h,enemy.SS:getDimensions()))
+    table.insert(enemy.quads,love.graphics.newQuad(x*enemy.sprite.h,0,enemy.sprite.w,enemy.sprite.h,enemy.SS:getDimensions()))
   end
 end
 
--- lib.lua
+++ lib.lua
@@ -9,3 +9,13 @@ end
 function distSq(x1, y1, x2, y2)
   return math.pow(x1-x2, 2) + math.pow(y1-y2, 2)
 end
+
+function clamp(x,mx)
+    
+        if math.abs(x) > math.abs(mx) then
+            return mx
+        else
+            return x
+        end
+    end
+    
