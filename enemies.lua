7a7b136c4d3f42c416011e74d3081548935ba7bb
James Rhodes <jarhodes314@gmail.com>
2017-12-01 14:19:58 +0000

    Enemies now show up!!

--- enemies.lua
+++ enemies.lua
@@ -1,5 +1,24 @@
 enemies = {}
 
-function love.load()
-  --some spritesheet for pufferfish
+function enemyLoad()
+  enemy = {SS = love.graphics.newImage("assets/img/enemies/fishbig.png")}
+  enemy.quads = {}
+  enemy.sprite = {w = 54, h = 49} 
+  enemy.drawCount = 0
+
+  enemies = {}
+
+  table.insert(enemies, {x = 50, y = 50})
+
+  for x=0, 3 do
+    table.insert(enemy.quads,love.graphics.newQuad(x*54,0,enemy.sprite.w,enemy.sprite.h,enemySprites:getDimensions()))
+  end
+end
+
+function enemyDraw()
+  enemy.drawCount = enemy.drawCount + 1
+  for i=#enemies, 1, -1 do
+    thisEnemy = enemies[i]  
+    love.graphics.draw(enemy.SS, enemy.quads[enemy.drawCount//4 + 1], thisEnemy.x, thisEnemy.y)
+  end
 end
