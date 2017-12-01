6aaa65b033cbb7eac095f01522ec015e5dde1710
Anik Roy <anik545@gmail.com>
2017-12-01 19:49:22 +0000

    .

---
+++ bullets.lua
@@ -0,0 +1,48 @@
+
+function bulletLoad()
+    bullets = {}
+    BULLET_VEL = 20
+    lastBullet = 0
+end
+
+function bulletUpdate(enemies)
+    
+    for i=#bullets, 1, -1 do
+        for j=#enemies, 1, -1 do
+            if objCollision(bullets[i], enemies[j]) then
+                table.remove(enemies,j)
+            end
+        end
+    end
+
+    for i=#bullets, 1, -1 do 
+        bullets[i].x = bullets[i].x + bullets[i].dir * BULLET_VEL
+    end
+
+    if love.keyboard.isDown("q") then
+        addBullet(player.dir, player.x, player.y)
+    end
+
+end
+
+function addBullet(d, startX, startY)
+    time = love.timer.getTime()    
+    print(time - lastBullet)
+    if time - lastBullet > 0.5 then
+        table.insert(bullets, {x=startX,y=startY,dir=d, width=5, height=5})        
+        lastBullet =love.timer.getTime()    
+        
+    end
+    
+
+end
+
+
+function bulletDraw()
+    love.graphics.setColor(255,100,255)
+    for i=1, #bullets do
+        love.graphics.rectangle("fill", bullets[i].x,bullets[i].y, bullets[i].height, bullets[i].width)
+    end
+    love.graphics.setColor(255,255,255)
+
+end
